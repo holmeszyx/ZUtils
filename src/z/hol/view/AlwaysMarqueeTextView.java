d7ef99577462144bfdaf0db897c3f37121956940
holmeszyx <holmeszyx@gmail.com>
2012-06-27 16:13:32 +0800

    修改跑马灯TextView

--- src/z/hol/view/AlwaysMarqueeTextView.java
+++ src/z/hol/view/AlwaysMarqueeTextView.java
@@ -1,6 +1,7 @@
 package z.hol.view;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.text.TextUtils.TruncateAt;
 import android.util.AttributeSet;
 import android.widget.TextView;
@@ -15,30 +16,40 @@ public class AlwaysMarqueeTextView extends TextView{
 	public AlwaysMarqueeTextView(Context context, AttributeSet attrs,
 			int defStyle) {
 		super(context, attrs, defStyle);
-		// TODO Auto-generated constructor stub
 		init();
 	}
 
 	public AlwaysMarqueeTextView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
 		init();
 	}
 
 	public AlwaysMarqueeTextView(Context context) {
 		super(context);
-		// TODO Auto-generated constructor stub
 		init();
 	}
 	
 	private void init(){
 		setSingleLine(true);
 		setEllipsize(TruncateAt.MARQUEE);
+		setMarqueeRepeatLimit(-1);
+	}
+	
+	@Override
+	protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
+	    if(focused)
+	        super.onFocusChanged(focused, direction, previouslyFocusedRect);
+	}
+	
+	@Override
+	public void onWindowFocusChanged(boolean hasWindowFocus) {
+		// TODO Auto-generated method stub
+		   if(hasWindowFocus)
+		        super.onWindowFocusChanged(hasWindowFocus);
 	}
 
 	@Override
 	public boolean isFocused() {
-		// TODO Auto-generated method stub
 		//return super.isFocused();
 		return true;
 	}
