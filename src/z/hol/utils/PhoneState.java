7efca77a38cf86747e6d9187a7adfedadf7895fb
holmeszyx <holmeszyx@gmail.com>
2012-08-23 17:44:24 +0800

    加入App版本获取，和一个文件操作类.加入文字剩余计数工具

--- src/z/hol/utils/PhoneState.java
+++ src/z/hol/utils/PhoneState.java
@@ -4,6 +4,14 @@ import android.content.Context;
 import android.telephony.TelephonyManager;
 
 public class PhoneState {
+	public static final int PHONE_TYPE_SIP = 3;
+	
+	public static final int NETWORK_TYPE_IDEN = 11;
+	public static final int NETWORK_TYPE_LTE = 13;
+	public static final int NETWORK_TYPE_EVDO_B = 12;
+	public static final int NETWORK_TYPE_EHRPD = 14;
+	public static final int NETWORK_TYPE_HSPAP = 15;
+	
 	TelephonyManager mTelephonyManager;
 	
 	private static PhoneState mPhoneState;
@@ -37,19 +45,84 @@ public class PhoneState {
 		String typeStr = null;
 		switch(type){
 		case TelephonyManager.PHONE_TYPE_CDMA:
-			typeStr = "CDMA";
+			typeStr = "cdma";
 			break;
 		case TelephonyManager.PHONE_TYPE_GSM:
-			typeStr = "GSM";
+			typeStr = "gsm";
 			break;
 		case TelephonyManager.PHONE_TYPE_NONE:
-			typeStr = "NONE";
+			typeStr = "none";
+			break;
+		case PHONE_TYPE_SIP:
+			typeStr = "sip";
 			break;
 		default:
-			typeStr = "UnKnow";
+			typeStr = "unknown";
 			break;
 		}
 		return typeStr; 
+	}
+	
+	public int getMobileNetType(){
+		return mTelephonyManager.getNetworkType();
+	}
+	
+	public static String mobileNetType2String(int type){
+		String str = null;
+		switch (type){
+		case TelephonyManager.NETWORK_TYPE_1xRTT:
+			str = "1xrtt";
+			break;
+		case TelephonyManager.NETWORK_TYPE_CDMA:
+			str = "cdma";
+			break;
+		case TelephonyManager.NETWORK_TYPE_EDGE:
+			str = "edge";
+			break;
+		case TelephonyManager.NETWORK_TYPE_EVDO_0:
+			str = "evdo_0";
+			break;
+		case TelephonyManager.NETWORK_TYPE_EVDO_A:
+			str = "evd0_a";
+			break;
+		case TelephonyManager.NETWORK_TYPE_GPRS:
+			str = "gprs";
+			break;
+		case TelephonyManager.NETWORK_TYPE_HSDPA:
+			str = "hsdpa";
+			break;
+		case TelephonyManager.NETWORK_TYPE_HSPA:
+			str = "hspa";
+			break;
+		case TelephonyManager.NETWORK_TYPE_HSUPA:
+			str = "hsupa";
+			break;
+		case TelephonyManager.NETWORK_TYPE_UMTS:
+			str = "umts";
+			break;
+		case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+			str = "unknown";
+			break;
+		case NETWORK_TYPE_EHRPD:
+			str = "ehrpd";
+			break;
+		case NETWORK_TYPE_EVDO_B:
+			str = "evdo_b";
+			break;
+		case NETWORK_TYPE_HSPAP:
+			str = "hspap";
+			break;
+		case NETWORK_TYPE_IDEN:
+			str = "iden";
+			break;
+		case NETWORK_TYPE_LTE:
+			str = "lte";
+			break;
+		default:
+			str = "unknown";
+			break;
+		}
+		return str;
 	}
 	
 	public String getNetworkOpertor(){
--- src/z/hol/view/OneRowGridView.java
+++ src/z/hol/view/OneRowGridView.java
@@ -11,7 +11,19 @@ import android.widget.GridView;
  */
 public class OneRowGridView extends GridView{
     private int mInternalRequestedColumnWidth;
+    
+    
 	
+	public OneRowGridView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	public OneRowGridView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
 	public OneRowGridView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		// TODO Auto-generated constructor stub
