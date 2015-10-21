4cec661b41282d9caa35fcb3d326b8d20b6c05b6
holmeszyx <holmeszyx@gmail.com>
2015-10-21 11:34:11 +0800

    系统asyncTask运行在无限线程池中.

--- src/z/hol/utils/ThreadUtils.java
+++ src/z/hol/utils/ThreadUtils.java
@@ -1,5 +1,13 @@
 package z.hol.utils;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import android.os.AsyncTask;
 import android.os.AsyncTask.Status;
 import android.os.HandlerThread;
@@ -8,6 +16,29 @@ import android.os.Process;
 public class ThreadUtils {
     
     public static final boolean IS_OLD_ASYNC_TASK = android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.HONEYCOMB;
+    
+    
+    private static final int CORE_POOL_SIZE = 2;
+    private static final int MAXIMUM_POOL_SIZE = 128;
+    private static final int KEEP_ALIVE = 3;
+    private static final ThreadFactory sThreadFactory = new ThreadFactory() {
+        private final AtomicInteger mCount = new AtomicInteger(1);
+
+        public Thread newThread(Runnable r) {
+            return new Thread(r, "ThreadUT #" + mCount.getAndIncrement());
+        }
+    };
+    private static final BlockingQueue<Runnable> sPoolWorkQueue = new SynchronousQueue<Runnable>();
+    /**
+     * An {@link Executor} that can be used to execute tasks in parallel.
+     * 核心线程数为{@link #CORE_POOL_SIZE}，不限制并发总线程数!
+     * 这就使得任务总能得到执行，且高效执行少量（<={@link #CORE_POOL_SIZE}）异步任务。
+     * 线程完成任务后保持{@link #KEEP_ALIVE}秒销毁，这段时间内可重用以应付短时间内较大量并发，提升性能。
+     * 它实际控制并执行线程任务。
+     */
+    public static final ThreadPoolExecutor THREAD_POOL_EXECUTOR = new ThreadPoolExecutor(CORE_POOL_SIZE,
+            MAXIMUM_POOL_SIZE, KEEP_ALIVE, TimeUnit.SECONDS, sPoolWorkQueue, sThreadFactory);
+
 
     /** 
      * 一个带looper的，后台线程。
@@ -60,7 +91,7 @@ public class ThreadUtils {
 	    if (IS_OLD_ASYNC_TASK){
 	        task.execute();
 	    }else{
-	        task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+	        task.executeOnExecutor(THREAD_POOL_EXECUTOR);
 	    }
 	}
 }
